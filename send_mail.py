4e589f14468e1b3ff6db1817308cb70f2a4d1f65
zombie12139 <969631475@qq.com>
2020-09-26 01:58:04 +0800

    支持多人打卡，修复一些bug

--- send_mail.py
+++ send_mail.py
@@ -14,8 +14,9 @@ def send_mail(mail_username, mail_password, mail_host, receiver, text, subject, 
 
         smtpObj = smtplib.SMTP_SSL(mail_host, 465)    # 25 为 SMTP 端口号 465是SMTP over SSL
         print('在login了')
+        print(mail_username, mail_password, )
         smtpObj.login(mail_username, mail_password)
-        print('在sned了')
+        print('send')
         smtpObj.sendmail(mail_username, [receiver], message.as_string())
         print('successfully send mail to ', receiver_name)
     except smtplib.SMTPException:
--- sign.py
+++ sign.py
@@ -4,27 +4,17 @@ import requests
 import time
 import json
 import re
+import json
 from send_mail import send_mail
 
-#---------------------------------------------#
-# your id and you password
-id = 'xxxxxxxxx'
-password = 'xxxxxxxxx'
-
-# 修改为你的邮箱账号、密码、smtp服务器地址
-# 建议使用QQ邮箱发送，163邮箱给QQ邮箱发送会发不出去
-mail_username = 'xxxxxxxxx@qq.com'
-mail_password = 'xxxxxxxxxxxxxxxxxx'
-mail_host = 'smtp.qq.com'
-
-# 收件人地址
-receiver = 'xxxxxxxxx@qq.com'
-#----------------------------------------------#
 
 # 如果发包过快会造成502，如果给多个同学打卡请注意一下请求速度
 try_times = 2
 
-# 抓的包直接复制的，打卡其实只要cookie和data就好了
+# 每次requests请求的延迟(s)，太低会封IP
+delay = 5
+
+# headers，所有的请求都用这个作为headers
 headers = {
     'Host': 'm.nuaa.edu.cn',
     'Connection': 'close',
@@ -46,17 +36,15 @@ headers = {
 post_data = 'sfzhux=0&zhuxdz=&szgj=&szcs=&szgjcs=&sfjwfh=0&sfyjsjwfh=0&sfjcjwfh=0&sflznjcjwfh=0&sflqjkm=0&jkmys=0&sfjtgfxdq=0&geo_api_info=%7B%22type%22%3A%22complete%22%2C%22info%22%3A%22SUCCESS%22%2C%22status%22%3A1%2C%22XDa%22%3A%22jsonp_951333_%22%2C%22position%22%3A%7B%22Q%22%3A31.94271%2C%22R%22%3A118.78832%2C%22lng%22%3A118.78832%2C%22lat%22%3A31.94271%7D%2C%22message%22%3A%22Get+ipLocation+success.Get+address+success.%22%2C%22location_type%22%3A%22ip%22%2C%22accuracy%22%3Anull%2C%22isConverted%22%3Atrue%2C%22addressComponent%22%3A%7B%22citycode%22%3A%22025%22%2C%22adcode%22%3A%22320115%22%2C%22businessAreas%22%3A%5B%7B%22name%22%3A%22%E5%BC%80%E5%8F%91%E5%8C%BA%22%2C%22id%22%3A%22320115%22%2C%22location%22%3A%7B%22Q%22%3A31.925973%2C%22R%22%3A118.80980399999999%2C%22lng%22%3A118.809804%2C%22lat%22%3A31.925973%7D%7D%5D%2C%22neighborhoodType%22%3A%22%22%2C%22neighborhood%22%3A%22%22%2C%22building%22%3A%22%22%2C%22buildingType%22%3A%22%22%2C%22street%22%3A%22%E8%83%9C%E5%A4%AA%E8%B7%AF%22%2C%22streetNumber%22%3A%22162%E5%8F%B7%22%2C%22country%22%3A%22%E4%B8%AD%E5%9B%BD%22%2C%22province%22%3A%22%E6%B1%9F%E8%8B%8F%E7%9C%81%22%2C%22city%22%3A%22%E5%8D%97%E4%BA%AC%E5%B8%82%22%2C%22district%22%3A%22%E6%B1%9F%E5%AE%81%E5%8C%BA%22%2C%22township%22%3A%22%E7%A7%A3%E9%99%B5%E8%A1%97%E9%81%93%22%7D%2C%22formattedAddress%22%3A%22%E6%B1%9F%E8%8B%8F%E7%9C%81%E5%8D%97%E4%BA%AC%E5%B8%82%E6%B1%9F%E5%AE%81%E5%8C%BA%E7%A7%A3%E9%99%B5%E8%A1%97%E9%81%93%E8%83%9C%E5%A4%AA%E8%B7%AF%E5%8D%97%E4%BA%AC%E8%88%AA%E7%A9%BA%E8%88%AA%E5%A4%A9%E5%A4%A7%E5%AD%A6%E5%B0%86%E5%86%9B%E8%B7%AF%E6%A0%A1%E5%8C%BA%22%2C%22roads%22%3A%5B%5D%2C%22crosses%22%3A%5B%5D%2C%22pois%22%3A%5B%5D%7D&date=2020{1:02}{2:02}&address=%E6%B1%9F%E8%8B%8F%E7%9C%81%E5%8D%97%E4%BA%AC%E5%B8%82%E6%B1%9F%E5%AE%81%E5%8C%BA%E7%A7%A3%E9%99%B5%E8%A1%97%E9%81%93%E8%83%9C%E5%A4%AA%E8%B7%AF%E5%8D%97%E4%BA%AC%E8%88%AA%E7%A9%BA%E8%88%AA%E5%A4%A9%E5%A4%A7%E5%AD%A6%E5%B0%86%E5%86%9B%E8%B7%AF%E6%A0%A1%E5%8C%BA&area=%E6%B1%9F%E8%8B%8F%E7%9C%81+%E5%8D%97%E4%BA%AC%E5%B8%82+%E6%B1%9F%E5%AE%81%E5%8C%BA&province=%E6%B1%9F%E8%8B%8F%E7%9C%81&city=%E5%8D%97%E4%BA%AC%E5%B8%82&fxzrwjtw=&fxjrcjtw=1&fxjrzjtw=&sfzx=1&sfcyglq=0&sfcxtz=0&uid=24113&created={0}&is_fx_log=1&id=9358620&gwszdd=&sfyqjzgc=&jcqzrq=&sfjcqz=&jrsfqzys=&jrsfqzfy=&szsqsfybl=0&sfsqhzjkk=&sqhzjkkys=&sfygtjzzfj=&gtjzzfjsj=&sftjwz=0&sftjhb=0&sfjcwhry=0&sfjchbry=0&ismoved=0'
 
 
-def sendmsg(msg):
-    if receiver != 'xxxxxxxxx@qq.com':
-        send_mail(mail_username, mail_password, mail_host, receiver, msg, msg, '童鞋', 'inuaa打卡姬')
 
-
-# 登陆获得cookie
-def login(login_id, login_password, login_headers):
+def login(login_id, login_password):
+    '''
+    登陆I南航，返回Cookie，失败返回空串
+    '''
     headers['Cookie'] = ''
     for _ in range(try_times):
         try:
-            time.sleep(0.5)
+            time.sleep(delay)
             r = requests.get(
                 'https://m.nuaa.edu.cn/uc/wap/login', headers=headers)
             print('get login page:', r.status_code)
@@ -66,9 +54,9 @@ def login(login_id, login_password, login_headers):
             # print(cookie)
             cookie = re.search(r'eai-sess=([a-zA-Z0-9]+)', cookie).group(0)
 
-            login_headers['Cookie'] = cookie
+            headers['Cookie'] = cookie
 
-            time.sleep(0.5)
+            time.sleep(delay)
             r = requests.get('https://m.nuaa.edu.cn/uc/wap/login/check', headers=headers,
                              data='username={}&password={}'.format(login_id, login_password))
             print('login...:', r.status_code)
@@ -77,50 +65,111 @@ def login(login_id, login_password, login_headers):
             cookie = cookie + '; ' + \
                 re.search(r'UUkey=([a-zA-Z0-9]+)', cookie2).group(0)
 
-            login_headers['Cookie'] = cookie
+            # headers['Cookie'] = cookie
             print(cookie)
-            return
+            return cookie
         except:
-            print('login failed')
+            print('login failed.')
             pass
+    # raise Exception('lOGIN FAIL')
+    return ''
 
-# 签到
-def sign(m, d):
+
+def sign(m, d, user, smtp_host, mail_username, mail_password):
+
+    '''
+    签到，并且发送邮件提醒，成功返回True，失败返回False
+    '''
     for _ in range(try_times):
         try:
-            time.sleep(0.5)
+            time.sleep(delay)
+            headers['Cookie'] = user['cookie']
             r = requests.post('https://m.nuaa.edu.cn/ncov/wap/default/save',
                               data=post_data.format(int(time.time()), m, d - 1), headers=headers)
-            print('statue code:', r.status_code)
-            print('return:', r.text)
-            if r.text == '{"e":0,"m":"操作成功","d":{}}':
-                sendmsg('打卡成功')
+            print('sign statue code:', r.status_code)
+            print('sign return:', r.text)
+            r.encoding = 'utf-8'
+            
+            if r.text.find('成功') >= 0:
+                print('打卡成功')
+                if user['receiver_mail'] != '':
+                    send_mail(mail_username, mail_password, smtp_host,
+                              user['receiver_mail'], '打卡成功', '打卡成功', user['name'], '打卡姬')
                 return True
             else:
-                login(id, password, headers)
+                print('打卡失败，尝试重新登陆')
+                user['cookie'] = login(id, password)
         except:
-            print('except')
+            print('尝试失败')
             pass
             # print(r.request.body)
-    sendmsg('打卡失败')
+    if user['receiver_mail'] != '':
+        send_mail(mail_username, mail_password, smtp_host,
+                  user['receiver_mail'], '打卡失败', '打卡事变', user['name'], '打卡姬')
     return False
 
 
-if __name__ == '__main__':
-    print('Start...')
+def main():
+    print('------>>>---->启动中<------<<<----')
     last_post = 10086   # 最后一次签到的日期
-    login(id, password, headers)
+    
+    # 读取配置文件
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+    smtp_host = config['smtp_host']
+    mail_password = config['mail_password']
+    mail_username = config['mail_username']
+    users = config['users']
+    
+    # 一起登陆啊，失败了就先空着，等打卡时候再来管他
+    for user in users:
+        print('Login...:', user['name'])
+        user['cookie'] = login(user['student_id'], user['student_password'])
 
+    
+    # 每一天先拷贝一下需要打卡的列表，然后打卡
+    # 其他时间如果监测到待打卡的列表非空，就重新读打卡
+    to_sign_list = users.copy()
     while True:
         t = time.localtime()
-        print('{0:02}:{1:02}:{2:02}\r'.format(
-            t.tm_hour, t.tm_min, t.tm_sec), end='')
+
         if t.tm_mday != last_post:
-            print('')
-            if sign(t.tm_mon, t.tm_mday):
-                print('Sign for {}.{} successfully!'.format(t.tm_mon, t.tm_mday))
-                last_post = t.tm_mday
-            else:
-                print('Sign for {}.{} failed!'.format(t.tm_mon, t.tm_mday))
-                time.sleep(3600)    # error wait for 5.55 h
+            # 新的一天，拷贝一份完整的打卡清单，全部打一遍卡。但是这样做的话每次更新cookie，users也自动更新。
+
+            print('----------开始每日打卡----------')
+            to_sign_list = users.copy()
+
+            # 给每个人打卡
+            for user in to_sign_list:
+                print('**********' + user['name'] + '**********')
+                if sign(t.tm_mon, t.tm_mday, user, smtp_host, mail_username, mail_password):
+                    print('Sign for {} on {}.{} successfully!'.format(
+                        user['name'], t.tm_mon, t.tm_mday))
+                    to_sign_list.remove(user)   # 移除打卡成功的用户
+                else:
+                    print('Sign for {} on {}.{} failed!'.format(
+                        user['name'], t.tm_mon, t.tm_mday))
+
+            last_post = t.tm_mday   # 更新日期
+
+        elif len(to_sign_list) != 0:
+            # 一天中的后续尝试，先等待2小时，然后再打卡
+            time.sleep(3600)    # 失败用户每一个小时重试一次
+        
+            print('----------重新打卡尝试----------')
+            # 给每个失败的人打卡
+            for user in to_sign_list:
+                print('**********' + user['name'] + '**********')
+                if sign(t.tm_mon, t.tm_mday, user, smtp_host, mail_username, mail_password):
+                    print('Sign for {} on {}.{} successfully!'.format(
+                        user['name'], t.tm_mon, t.tm_mday))
+                    to_sign_list.remove(user)   # 移除打卡成功的用户
+                else:
+                    print('Sign for {} on {}.{} failed!'.format(
+                        user['name'], t.tm_mon, t.tm_mday))
+
+        # else: 都打完了
         time.sleep(1)
+
+if __name__ == '__main__':
+    main()
